f3e8f83776efc6dfb675caf659df3e01ca3e03f6
fatou7y <silver.key1710@gmail.com>
2023-06-12 11:41:56 +0300

    intial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+var = {'C:/Users/abdel/Desktop/CSV': ['CSV', None],
+       'C:/Users/abdel/Desktop/CSV/Dir 1': ['Dir 1', 'C:/Users/abdel/Desktop/CSV'],
+       'C:/Users/abdel/Desktop/CSV/1.csv': ['1.csv', 'C:/Users/abdel/Desktop/CSV'],
+       'C:/Users/abdel/Desktop/CSV/2.csv': ['2.csv', 'C:/Users/abdel/Desktop/CSV'],
+       'C:/Users/abdel/Desktop/CSV/3.csv': ['3.csv', 'C:/Users/abdel/Desktop/CSV']}
+
+
